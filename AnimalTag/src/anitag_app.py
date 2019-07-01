42562e9c912eee87885d87b11f4dfe9f1c45ca28
fanxia <fanxia08@gmail.com>
2019-07-01 19:35:59 -0400

    updates

--- AnimalTag/src/anitag_app.py
+++ AnimalTag/src/anitag_app.py
@@ -37,9 +37,9 @@ def start_publisher(yvideo,thrds=3):
     vfile=yvideo.streams.first()
     vfile.download(output_path=ydir)
     filename='{0}/{1}'.format(ydir,vfile.default_filename)
-    #publisher.publisher(channel,cfg["rabbitmq"]["mq_name"],filename)
-    for i in range(thrds):
-        multiprocessing.Process(target=publisher.publisher,args=[channel,cfg["rabbitmq"]["mq_name"],filename,i,thrds]).start()
+    publisher.publisher(channel,cfg["rabbitmq"]["mq_name"],filename)
+    #for i in range(thrds):
+    #    multiprocessing.Process(target=publisher.publisher,args=[channel,cfg["rabbitmq"]["mq_name"],filename,i,thrds]).start()
 
 
 app=dash.Dash(__name__, external_stylesheets=['https://codepen.io/chriddyp/pen/bWLwgP.css'])
